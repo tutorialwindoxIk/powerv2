9a33c68cb584f4896b8466e343348733cdff4a91
alonxbad <167286922+alonxbadmusic@users.noreply.github.com>
2024-10-05 03:54:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/alonxbadmusic/Alonxbad007)
+    "UPSTREAM_REPO", "https://github.com/alonxbadmusic/Alonxbad007.git)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
