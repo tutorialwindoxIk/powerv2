6ba656da86856af0c0a85faa3b864fd6a10f5416
EXPERT CODER <150228624+TMM-TEAM@users.noreply.github.com>
2024-10-06 22:29:15 +0530

    Update radio.py

--- SlayerX/plugins/play/radio.py
+++ SlayerX/plugins/play/radio.py
@@ -12,17 +12,17 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, adminlist
 from strings import get_string
-from AarohiX import app
-from AarohiX.misc import SUDOERS
-from AarohiX.utils.database import (
+from SlayerX import app
+from SlayerX.misc import SUDOERS
+from SlayerX.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
     get_playmode,
     get_playtype,
 )
-from AarohiX.utils.logger import play_logs
-from AarohiX.utils.stream.stream import stream
+from SlayerX.utils.logger import play_logs
+from SlayerX.utils.stream.stream import stream
 
 RADIO_STATION = {
     "Air Bilaspur": "http://air.pc.cdn.bitgravity.com/air/live/pbaudio110/playlist.m3u8",
